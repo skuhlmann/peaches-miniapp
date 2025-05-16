72aee92621aac9ffa999fa2170c4c33174a144e8
skuhlmann <samkuhlmann@gmail.com>
2025-05-16 10:00:28 -0600

    base contracts

--- components/providers.tsx
+++ components/providers.tsx
@@ -7,7 +7,7 @@ import dynamic from "next/dynamic";
 import farcasterFrame from "@farcaster/frame-wagmi-connector";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { createConfig, http, WagmiProvider } from "wagmi";
-import { base, sepolia } from "wagmi/chains";
+import { base } from "wagmi/chains";
 
 import { injected } from "wagmi/connectors";
 import { HAUS_RPC_DEFAULTS } from "@/lib/constants";
@@ -18,11 +18,11 @@ const ErudaProvider = dynamic(
 );
 
 export const config = createConfig({
-  // chains: [base],
-  chains: [sepolia],
+  chains: [base],
+  // chains: [sepolia],
   transports: {
-    // [base.id]: http(HAUS_RPC_DEFAULTS["0x2105"]),
-    [sepolia.id]: http(HAUS_RPC_DEFAULTS["0xaa36a7"]),
+    [base.id]: http(HAUS_RPC_DEFAULTS["0x2105"]),
+    // [sepolia.id]: http(HAUS_RPC_DEFAULTS["0xaa36a7"]),
   },
   connectors: [farcasterFrame(), injected()],
 });
@@ -36,8 +36,8 @@ export default function Providers({ children }: { children: React.ReactNode }) {
         projectId={env.NEXT_PUBLIC_MINIKIT_PROJECT_ID}
         notificationProxyUrl="/api/notification"
         // @ts-expect-error
-        // chain={base}
-        chain={sepolia}
+        // chain={sepolia}
+        chain={base}
       >
         <WagmiProvider config={config}>
           <QueryClientProvider client={queryClient}>
